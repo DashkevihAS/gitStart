0d821969c9313413469fb92908e76065a20df97a
Anton <anton_snov@mail.ru>
2022-04-07 13:02:30 +0300

    day2

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,12 +2,5 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
-;// CONCATENATED MODULE: ./src/js/modules/test.js
-var sum = function sum(a, b) {
-  return a + b;
-};
-;// CONCATENATED MODULE: ./src/js/index.js
-
-console.log(sum(2, 4));
 /******/ })()
 ;
--- src/js/index.js
+++
@@ -1,3 +0,0 @@
-import {sum} from './modules/test.js';
-
-console.log(sum(2,4));
